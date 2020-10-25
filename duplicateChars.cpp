bdac54ee40202339288406544f22bfb3bec8633e
arikiv <59433949+point-contents@users.noreply.github.com>
2020-10-26 03:25:08 +1300

    working better ish

--- duplicateChars.cpp
+++ duplicateChars.cpp
@@ -3,15 +3,21 @@
 #include <cctype>
 #include <set>
 #include <algorithm>
+#include <assert.h>
+
 
 void duplicates(std::string& word)
 {
 
+	std::cout << "Input = " << word << "\n";
+	//
 	//make lowercase
 	
 	std::transform(word.begin(), word.end(), word.begin(),
 			[](unsigned char c){return std::tolower(c); });
 
+//	assert(word='1');
+//
 	//make a set for both the characters and the duplicates.
 	//sets cannot have duplicates, so there will only be one of each
 
@@ -35,35 +41,66 @@ void duplicates(std::string& word)
 		}
 	}
 
-	for(std::set<char>::const_iterator it = duplicates.begin(); it != duplicates.end(); it++)
+	for(std::string::size_type i = 0; i < word.size(); i++)
 	{
-		for(std::string::size_type i = 0; i < word.size(); i++)
+
+		for(std::set<char>::const_iterator it = duplicates.begin(); it != duplicates.end(); it++)
 		{
 			if(*it == word[i])
 			{
-				std::replace(word.begin(), word.end(), word[i], ")");
-			}	
-		}	
+				char moreThanOnce = ')';
+				word[i] = moreThanOnce ; 
+				assert(word[i]=')');
+			}
+			else if(*it != word[i])
+			{
+				char once = '(';
+				word[i] = once;
+				assert(word[i]='(');
+				std::cout << word[i];
+			}
+		}
 	}
 
+	// Need to work out how to do the same thing for characters
+	// that arent in the duplicated set 
+	
+//	for(std::string::size_type i = 0; i < word.size(); i++)
+//	{
+//
+//		for(std::set<char>::const_iterator it = duplicates.begin(); it != duplicates.end(); it++)
+//		{
+//			if(*it == word[i])
+//			{
+//				char moreThanOnce = ')';
+//				word[i] = moreThanOnce ; 
+//				assert(word[i]=')');
+//			}
+//		}
+//	}
+	std::cout << "Answer = ?? " << "\n";
+	std::cout << word << "\n";
 
 	//
-//	std::cout << "Repeated: " << "\n";
-//	for(std::set<char>::const_iterator it = duplicates.begin(); it != duplicates.end(); it++)
-//	{
-//		std::cout << *it << " ";
-//	}
-//
-//	std::cout << "\n" << "Unique Characters" << "\n";
-//	for(std::set<char>::const_iterator it = characters.begin(); it != characters.end(); it++)
-//	{
-//		std::cout << *it << " ";
-//	}
+	std::cout << "Repeated: " << "\n";
+	for(std::set<char>::const_iterator it = duplicates.begin(); it != duplicates.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+
+	std::cout << "\n" << "Unique Characters" << "\n";
+	for(std::set<char>::const_iterator it = characters.begin(); it != characters.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+
 }
 
 int main()
 {
-	std::string input = "Hello There is notmuch that we can do here ";
+	std::string input; 
+	std::cout << "\n" << "Add input, duplicate values will be ')'" << "\n";
+	std::getline(std::cin, input);
 	duplicates(input);
 
 	return 0;
