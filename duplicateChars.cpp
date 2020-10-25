31d94f9adf0bdb0263ced1588414cb7e83ad1da8
arikiv <59433949+point-contents@users.noreply.github.com>
2020-10-26 11:50:59 +1300

    separate the functions up, weirdly it is working on my computer, but not when I try their tests, I think it might be something to do with "word" being a const in the normal test

--- duplicateChars.cpp
+++ duplicateChars.cpp
@@ -6,16 +6,36 @@
 #include <assert.h>
 #include <vector>
 
-
-void duplicates(std::string& word)
+std::string lowerIt(std::string& upper)
 {
-
-	std::cout << "Input = " << word << "\n";
-	//
-	//make lowercase
+	//make string lowercase
+	std::string word = upper;
 	
 	std::transform(word.begin(), word.end(), word.begin(),
 			[](unsigned char c){return std::tolower(c); });
+	
+	return word;
+}
+
+std::vector<char> leftJoin(std::set<char>& characters, std::set<char>& duplicates)
+{
+
+	//make a set of ones that are not duplicated,
+	//these ones will be replaced by "(" in the final string
+
+	std::vector<char> notDuplicated;
+
+	std::set_difference(characters.begin(), characters.end(),
+						duplicates.begin(), duplicates.end(),
+						std::inserter(notDuplicated, notDuplicated.begin()));	
+
+	return notDuplicated;
+}
+
+std::string duplicates(std::string& word)
+{
+
+	word = lowerIt(word);
 
 	//make a set for both the characters and the duplicates.
 	//sets cannot have duplicates, so there will only be one of each
@@ -24,6 +44,9 @@ void duplicates(std::string& word)
 	std::set<char> duplicates;
 	std::vector<char> notDuplicated;
 
+	//loop throught the string, do not understand what the 
+	//static cast is doing here exactly.
+	
 	for(std::string::size_type i = 0; i < word.size(); i++)
 	{
 		char c = word[i];
@@ -41,19 +64,7 @@ void duplicates(std::string& word)
 		}
 	}
 
-	std::set_difference(characters.begin(), characters.end(),
-						duplicates.begin(), duplicates.end(),
-						std::inserter(notDuplicated, notDuplicated.begin()));	
-
-
-	if(characters.size() > duplicates.size())
-	{
-		assert(notDuplicated.size() >= 0);
-	}
-	else
-	{
-		assert(notDuplicated.size() == 0);
-	}
+	notDuplicated = leftJoin(characters, duplicates);
 
 	for(std::string::size_type i = 0; i < word.size(); i++)
 	{
@@ -85,30 +96,7 @@ void duplicates(std::string& word)
 			}
 		}
 	}
-
-	std::cout << "Answer = ?? " << "\n";
-	std::cout << word << "\n";
-
-
-	std::cout << "Repeated: " << "\n";
-	for(std::set<char>::const_iterator it = duplicates.begin(); it != duplicates.end(); it++)
-	{
-		std::cout << *it << " ";
-	}
-
-	std::cout << "\n" << "Unique Characters" << "\n";
-	for(std::set<char>::const_iterator it = characters.begin(); it != characters.end(); it++)
-	{
-		std::cout << *it << " ";
-	}
-
-	std::cout << "\n" << "Non Duplicated: " << "\n";
-	
-	for(int i = 0; i < notDuplicated.size(); i++)
-	{
-		std::cout << notDuplicated[i] << ", ";
-	}
-	//loop through non duplicated array
+	return word;
 }
 
 int main()
@@ -116,7 +104,7 @@ int main()
 	std::string input; 
 	std::cout << "\n" << "Add input, duplicate values will be ')'" << "\n";
 	std::getline(std::cin, input);
-	duplicates(input);
+	std::cout << duplicates(input);
 
 	return 0;
 }
