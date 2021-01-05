0e4660568f42399932286852791ef2259f4dd6e2
person <volkerarik@myvuw.ac.nz>
2021-01-05 16:40:13 +1300

    level 8, quite simple

---
+++ cpp/countingSheep/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int count_sheep(std::vector<bool> arr)
+{
+	int counter = 0;
+	for(auto sheep: arr)
+	{
+		if(sheep == true)
+		{
+			counter++;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return counter;
+
+}
+
+int main()
+{
+
+	return 0;
+}
