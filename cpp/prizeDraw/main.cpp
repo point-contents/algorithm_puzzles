8eac3749c6a4bdd6c2af8c55c8e868e6785deaf7
arikiv <59433949+point-contents@users.noreply.github.com>
2021-01-07 22:31:54 +1300

    so so close, there is a small mistake somewhere, but the gist of it seems right

---
+++ cpp/prizeDraw/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+std::vector<std::string> tokenizeString(std::string s)
+{
+	std::vector<std::string> tokenizedVector;
+	std::string delimiter = ",";
+	size_t pos = 0;
+	std::string token;
+	while ((pos = s.find(delimiter)) != std::string::npos) {
+		token = s.substr(0, pos);
+		tokenizedVector.push_back(token);
+		s.erase(0, pos + delimiter.length());
+	}
+	tokenizedVector.push_back(s);
+	return tokenizedVector;
+}
+
+int calculateNameTotal(std::string s, int weight)
+{
+	int subTotal = s.length();	
+	int total = 0; 
+	for(auto& letters: s)
+	{
+		int value;
+		value = (int)((std::tolower(letters)) - 65);
+		subTotal += value;
+		total = subTotal * weight;
+	}
+
+	return total;
+}
+
+bool sortPairByValue(const std::pair<std::string,int>& a,const std::pair<std::string,int>& b) 
+{ 
+    return (a.second < b.second); 
+} 
+
+std::string nthRank(const std::string &st, std::vector<int> &we, int n)
+{
+	if(st.size() == 0)
+	{
+		return "No participants";
+	}
+	else
+	{
+		std::vector<std::pair<std::string,int>> totals;
+		std::vector<std::string> names = tokenizeString(st);
+		std::vector<int>::iterator weightIt = we.begin();
+		for(auto& people: names)
+		{
+			int score = calculateNameTotal(people, *weightIt);
+			totals.push_back(std::pair<std::string,int>(people,score));
+//			std::cout << people << " " << score << std::endl;
+			weightIt++;
+		}
+
+//		std::cout << "\n\nPre-sorted" << std::endl;
+//		for(auto& things:totals)
+//			std::cout << things.first << " " << things.second << std::endl;
+//
+		std::sort(totals.begin(), totals.end(), sortPairByValue);
+
+		std::cout << "\n\nPost-Sorted" << std::endl;
+		for(auto& things:totals)
+			std::cout << things.first << " " << things.second << std::endl;
+	
+		return totals[n-1].first;
+	}
+}
+
+int main()
+{
+//	    std::string st = "Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin";
+//        std::vector<int> we = {4, 2, 1, 4, 3, 1, 2};
+//        std::string sol = "Benjamin";
+
+		auto st = "Elijah,Chloe,Elizabeth,Matthew,Natalie,Jayden";
+		std::vector<int> we = {1, 3, 5, 5, 3, 6};
+		std::string sol = "Matthew";
+		std::cout << nthRank(st,we,4) << std::endl;
+	return 0;
+}
