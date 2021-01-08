7c35a48692dc8695503c6a7ba87cfa4cea113505
arikiv <59433949+point-contents@users.noreply.github.com>
2021-01-08 17:22:23 +1300

    easier one

---
+++ cpp/reverseNumber/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+
+std::vector<int> digitize(unsigned long n) {
+	std::vector<int> answer;
+	while(n > 0)
+	{
+		int digit = n % 10;
+		n /= 10;
+		answer.push_back(digit);
+	}
+	return answer;
+}
+
+void printVector(std::vector<int> input)
+{
+	for(auto& shit: input)
+		std::cout << shit << "," << "\n";
+
+
+}
+int main() 
+{ 
+	int a = 1234;
+	auto ans = digitize(a);	
+	printVector(ans);
+
+	return 0; 
+}
