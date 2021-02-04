841d1961571ba3a6b839cff755f3b64ad649c14c
arikiv <59433949+point-contents@users.noreply.github.com>
2021-02-04 13:12:05 +1300

    cheated, there was a better version, which was the way i was trying to do it, I will copy in their solution, and try to understand why my one wasnt working

---
+++ cpp/validBraces/main2.cpp
@@ -0,0 +1,61 @@
+#include <stack>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <map>
+
+std::map<char,char> bracePairs()
+{
+	std::map<char,char> pairs =
+	{
+		{'}','{'},
+		{')','('},
+		{']','['}
+	};
+
+	return pairs;
+}
+
+
+bool validBraces(std::string braces)
+{
+	std::stack<char> braceStack;
+	std::map<char,char> pairs = bracePairs();
+
+	for(int i = 0; i < braces.size(); i++)
+	{
+		if(braces[i] == '(' || braces[i] == '[' || braces[i] == '{')
+		{
+			braceStack.push(braces[i]);
+		}
+		else
+		{
+			if(braceStack.empty())
+			{
+				return false;
+			}
+			else
+			{
+				char top = braceStack.top();
+				if(braces[i] == '}' && top == '{' ||
+				braces[i] == ']' && top == '[' ||
+				braces[i] == ')' && top == '(') 
+				{
+					braceStack.pop();
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+	}
+	return braceStack.empty();
+}
+
+int main()
+{
+	std::cout << validBraces("()()");
+
+	return 0;
+}
