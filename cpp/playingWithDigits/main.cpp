63bcfdc7c168a47a408feb5cfae09d6a5366f1b7
arikiv <59433949+point-contents@users.noreply.github.com>
2021-02-04 18:11:35 +1300

    half way, this is not the way to do it though I think

---
+++ cpp/playingWithDigits/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <assert.h>
+
+
+#define LOG(x) std::cout << x << "\n";
+
+template<typename T> void printVec(T vec)
+{
+	for(auto& things: vec)
+	{
+		std::cout << things << "\n";
+	}
+}
+
+std::vector<int> getDigits(int n)
+{
+	std::vector<int> digits;
+	while(n != 0)
+	{
+		digits.push_back(n % 10);
+		n /= 10;
+	}
+	std::cout << "Digit Size: " << digits.size() << std::endl;
+	std::reverse(digits.begin(), digits.end());
+	return digits;
+}
+
+static int digPow(int n, int p)
+{
+	auto digits = getDigits(n);	
+	long long int sum;
+	for(int i = 0; i < digits.size(); i++)
+	{
+		sum += pow(digits[i],p);
+		p++;
+	}
+	return sum;
+}
+
+int main()
+{
+	std::cout << digPow(695, 2);
+	return 0;
+}
