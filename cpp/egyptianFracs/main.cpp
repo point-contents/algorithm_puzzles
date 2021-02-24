83147baebfaa3a10a4f28c30fa52a3f40e5d1374
arikiv <59433949+point-contents@users.noreply.github.com>
2021-02-24 20:37:55 +1300

    start this, mostly formatting shit"

---
+++ cpp/egyptianFracs/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <assert.h>
+
+
+#define LOG(x) std::cout << x << "\n";
+
+template<typename T> void printVec(T vec)
+{
+	for(auto& things: vec)
+	{
+		std::cout << things << "\n";
+	}
+}
+
+class Decomp
+{
+	double numer, denom;
+	float total;
+public:
+  void calcTotal(const std::string &nrStr, const std::string &drStr)
+  {
+	numer = std::stod(nrStr);
+	denom = std::stod(drStr);
+	total = numer / denom;
+  }
+
+  void printTotal() { LOG(total) };
+
+  std::string decompose(const std::string &nrStr, const std::string &drStr)
+  {
+	calcTotal(nrStr, drStr);
+
+	return "";
+  }
+};
+
+int main(int argc, char* argv[])
+{
+	Decomp calc;
+	calc.decompose("10", "100");
+	calc.printTotal();
+	return 0;
+}
