57b26454d2e03439309dfb98e2cc62240da2ce64
arikiv <59433949+point-contents@users.noreply.github.com>
2021-02-24 21:34:52 +1300

    meh

---
+++ cpp/egyptianFracs/main3.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <assert.h>
+
+
+#define LOG(x) std::cout << x << "\n";
+
+template<typename T> void printVec(T vec)
+{
+	for(auto& things: vec)
+	{
+		std::cout << things << "\n";
+	}
+}
+
+using namespace std;
+
+void printEgyptian(int nr, int dr) 
+{ 
+    if (dr == 0 || nr == 0) 
+        return; 
+  
+    // If numerator divides denominator, then simple division 
+    // makes the fraction in 1/n form 
+    if (dr % nr == 0) 
+    { 
+        cout << "1/" << dr/nr; 
+        return; 
+    } 
+  
+    // If denominator divides numerator, then the given number 
+    // is not fraction 
+    if (nr%dr == 0) 
+    { 
+        cout << nr/dr; 
+        return; 
+    } 
+  
+    // If numerator is more than denominator 
+    if (nr > dr) 
+    { 
+        cout << nr/dr << " + "; 
+        printEgyptian(nr%dr, dr); 
+        return; 
+    } 
+  
+    // We reach here dr > nr and dr%nr is non-zero 
+    // Find ceiling of dr/nr and print it as first 
+    // fraction 
+    int n = dr/nr + 1; 
+    cout << "1/" << n << " + "; 
+  
+    // Recur for remaining part 
+    printEgyptian(nr*n-dr, dr*n); 
+ }
+
+int main(int argc, char* argv[])
+{
+	printEgyptian(100, 25023498);
+	return 0;
+}
