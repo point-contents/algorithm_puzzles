ffc4af931c022759441e13a1b4071d074429e7e0
me <amanvolly@gmail.com>
2021-03-29 14:09:17 +1300

    add a bash script to make a new entry, and automatically add it to the cmakelists folder for building later

---
+++ cWars/README.md
@@ -0,0 +1,4 @@
+Running the bash file "makenew.sh" will create a new folder with the given argument
+as the folder name, create an entry in the cmakelist for the whole directory, and 
+make a new boilerplate cpp file in the created directory
+
--- makeme.sh
+++ makeme.sh
@@ -4,6 +4,8 @@ if [ $# -eq 0  ]; then
 	rm -rf build ; mkdir build ; cd build ; cmake .. ; make
 else
 	if [ $1 = "rebuild" ]; then
+		clang-format -style=Webkit -dump-config  > .clang-format
+		find . -iname "*.cpp" | xargs clang-format -i 
 		cd build ; make 
 	else
 		echo "I dont understand"
